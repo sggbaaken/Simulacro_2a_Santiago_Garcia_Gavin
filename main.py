d9414ecb058de50627f176ae72f6389fb59691dc
sggbaaken <206068814+sggbaaken@users.noreply.github.com>
2026-03-17 11:53:17 +0100

    Realizado el código en Python

---
+++ main.py
@@ -0,0 +1,12 @@
+numero1 = 3
+numero2 = 9
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+print (f"Suma = {suma}")
+print (f"Resta = {resta}")
+print (f"Multiplicación = {multiplicacion}")
+print (f"División = {division}")
